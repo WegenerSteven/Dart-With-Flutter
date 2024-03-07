13195b799e11b96643bd70fd6c7055d8b3465c5c
John Bradley <jontexjesus@gmal.com>
2024-03-07 12:17:51 +0300

    Variables and concatination in Dart

---
+++ variables.dart
@@ -0,0 +1,15 @@
+//declaring variables in dart
+//we use var keyword
+void main() {
+  //declaring variable name and assigning Bradley to it.
+  String name = 'Bradley';
+  int age = 20;
+  //the final and const keywords are used to declare constants.
+  //Calculating area of a circle.
+  final Radius = 12;
+  const pi = 3.142;
+  var Area = Radius * Radius * pi;
+  //Concatitaning string
+  print("my name is $name and am $age years old now");
+  print("The Area of Circle is $Area ");
+}
