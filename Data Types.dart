f86471732daecb40416ac90ee854e38a9d6402ea
John Bradley <jontexjesus@gmal.com>
2024-03-10 23:43:40 +0300

    new commit

---
+++ Data Types.dart
@@ -0,0 +1,13 @@
+void main() {
+//numbers Data types.
+//declare variables with int ,double and assigning values to them.
+  int num1 = 5000;
+  double num2 = 2500.6768;
+  double Amount = num1 + num2;
+  print("The sum is $Amount");
+  //string data types
+  String name = 'Bradley';
+  //Booleans
+  bool isMarried = true;
+  print("She is alrady Married");
+}
